17e014fa8ab46ea7dbb4327acf9dc40cf515f4a1
evilvegetarian <vegetriane@yandex.ru>
2025-06-22 15:14:15 +0500

    Добавил некоторые компоненты из shadcn

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,8 +1,28 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import {createBrowserRouter, RouterProvider} from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    //element: <Layout />,
+    children: [
+      {
+        index: true,
+        //element: <SignOnPage />,
+      },
+      {
+        path: "posts/schedule",
+        //element: <TelegramChannelSchedulePage />,
+      },
+    ],
+    errorElement: <div>Page not found</div>,
+  },
+])
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-  </StrictMode>,
+    <StrictMode>
+      <RouterProvider router={router} />
+    </StrictMode>,
 )
