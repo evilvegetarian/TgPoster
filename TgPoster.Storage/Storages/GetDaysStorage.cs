708bd6503c2ba57da874f6d3745e7d21aa18638c
evilvegetarian <vegetriane@yandex.ru>
2025-12-14 19:49:50 +0500

    Добавил сортировку

--- TgPoster.Storage/Storages/GetDaysStorage.cs
+++ TgPoster.Storage/Storages/GetDaysStorage.cs
@@ -6,20 +6,27 @@ namespace TgPoster.Storage.Storages;
 
 internal sealed class GetDaysStorage(PosterContext context) : IGetDaysStorage
 {
-	public async Task<bool> ScheduleExistAsync(Guid scheduleId, Guid userId, CancellationToken ct)
+	public Task<bool> ScheduleExistAsync(Guid scheduleId, Guid userId, CancellationToken ct)
 	{
-		return await context.Schedules.AnyAsync(x => x.Id == scheduleId && x.UserId == userId, ct);
+		return context.Schedules.AnyAsync(x => x.Id == scheduleId && x.UserId == userId, ct);
 	}
 
 	public async Task<List<GetDaysResponse>> GetDaysAsync(Guid scheduleId, CancellationToken ct)
 	{
-		return await context.Days.Where(x => x.ScheduleId == scheduleId)
-			.Select(x => new GetDaysResponse
+		var daysFromDb = await context.Days
+			.Where(x => x.ScheduleId == scheduleId)
+			.OrderBy(x => x.DayOfWeek)
+			.ToListAsync(ct);
+
+		var result = daysFromDb.Select(x => new GetDaysResponse
 			{
 				Id = x.Id,
 				ScheduleId = x.ScheduleId,
 				DayOfWeek = x.DayOfWeek,
-				TimePostings = x.TimePostings
-			}).ToListAsync(ct);
+				TimePostings = x.TimePostings.OrderBy(t => t).ToList()
+			})
+			.ToList();
+
+		return result;
 	}
 }
