5d5bc1e37f033bb48592a4facef8c24289ffd4e6
evilvegetarian <vegetriane@yandex.ru>
2026-04-22 21:45:05 +0500

    Update ListTelegramSessionsStorage.cs

--- TgPoster.Storage/Storages/ListTelegramSessionsStorage.cs
+++ TgPoster.Storage/Storages/ListTelegramSessionsStorage.cs
@@ -7,19 +7,29 @@ namespace TgPoster.Storage.Storages;
 
 internal sealed class ListTelegramSessionsStorage(PosterContext context) : IListTelegramSessionsStorage
 {
-	public Task<List<TelegramSessionResponse>> GetByUserIdAsync(Guid userId, CancellationToken ct)
+	public async Task<List<TelegramSessionResponse>> GetByUserIdAsync(Guid userId, CancellationToken ct)
 	{
-		return context.TelegramSessions
+		var items = await context.TelegramSessions
 			.Where(s => s.UserId == userId)
 			.OrderByDescending(s => s.Created)
-			.Select(s => new TelegramSessionResponse(
+			.Select(s => new
+			{
 				s.Id,
 				s.PhoneNumber,
 				s.Name,
 				s.IsActive,
-				(TelegramSessionStatus)s.Status,
+				s.Status,
 				s.Created
-			))
+			})
 			.ToListAsync(ct);
+
+		return items.ConvertAll(s => new TelegramSessionResponse(
+			s.Id,
+			s.PhoneNumber,
+			s.Name,
+			s.IsActive,
+			(TelegramSessionStatus)s.Status,
+			s.Created
+		));
 	}
 }
