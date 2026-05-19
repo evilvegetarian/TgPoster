5bd307edf5c6df589c653f7d928b3ac5a1cfbd98
evilvegetarian <vegetriane@yandex.ru>
2026-05-19 21:04:14 +0500

    Update ClassificationOptions.cs

--- TgPoster.Worker.Domain/ConfigModels/ClassificationOptions.cs
+++ TgPoster.Worker.Domain/ConfigModels/ClassificationOptions.cs
@@ -3,6 +3,6 @@ namespace TgPoster.Worker.Domain.ConfigModels;
 public class OpenRouterOptions
 {
 	public string? SecretKey { get; set; }
-	public string Model { get; set; } = "x-ai/grok-4-fast";
+	public string Model { get; set; } = "x-ai/grok-4.3";
 	public int MessageSampleCount { get; set; } = 25;
 }
