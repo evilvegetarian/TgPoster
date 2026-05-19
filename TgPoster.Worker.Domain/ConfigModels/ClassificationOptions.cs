b2e9246cecb470f4a16eae86d20829977a1a3931
evilvegetarian <vegetriane@yandex.ru>
2026-05-19 23:06:26 +0500

    Update ClassificationOptions.cs

--- TgPoster.Worker.Domain/ConfigModels/ClassificationOptions.cs
+++ TgPoster.Worker.Domain/ConfigModels/ClassificationOptions.cs
@@ -3,6 +3,6 @@ namespace TgPoster.Worker.Domain.ConfigModels;
 public class OpenRouterOptions
 {
 	public string? SecretKey { get; set; }
-	public string Model { get; set; } = "qwen/qwen3-vl-8b";
+	public string Model { get; set; } = "qwen/qwen3-vl-8b-instruct";
 	public int MessageSampleCount { get; set; } = 25;
 }
